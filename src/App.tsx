ed9305b36cadcdc474fd95edae1148df271b242f
yoyohoho84 <kirill_1131@mail.ru>
2023-05-11 17:14:58 +0300

    Updates

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import {Home} from "./containers/pages/home";
-
-function App() {
-    return <Home />;
-}
-
-export default App;
--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-
-export const formatDate = (value: string | Date | null): string | null => {
-  return value ? moment(value).format('DD.MM.YYYY') : '';
-};
-
-export const formatDateUnix = (value: number): string => {
-  return moment.unix(value).format('HH:mm:ss');
-};
